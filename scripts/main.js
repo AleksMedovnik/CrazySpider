d67f257bdfbe72a2c7f85b40255782398ba16c32
Aleksandr <medovnikaleksander@yandex.ua>
2021-12-13 23:18:32 +0300

    Added anim

--- scripts/main.js
+++ scripts/main.js
@@ -1,78 +1,80 @@
 "use strict"
+window.onload = () => {
 
-window.onload = playAnimation;
+    const canvas = document.getElementById('canvas');
+    const ctx = canvas.getContext('2d');
 
-const main = {};
+    canvas.width = 500;
+    canvas.height = 300;
 
-
-
-function init() {
-    main.canvas = document.getElementById('canvas');
-    main.ctx = canvas.getContext('2d');
-
-    main.canvas.width = 500;
-    main.canvas.height = 300;
-
-    main.aim = {
-        x: main.canvas.width / 2,
-        y: main.canvas.height / 2
+    const aim = {
+        x: canvas.width / 2,
+        y: canvas.height / 2
     };
 
-    main.spider = new SpriteMG(Math.random() * (main.canvas.width - 200), Math.random() * (main.canvas.height - 150), 150, 150, 0, 0, 30, './images/spider.png');
- 
-    main.expl = new Image();
-    main.grassImg = new Image();
-    main.expl.src = './images/expl.jpg';
-    main.grassImg.src = './images/grass.jpg'
-    main.explosion = [];
+    let spider = new Spider(
+        Math.random() * (canvas.width - 200),
+        Math.random() * (canvas.height - 150),
+        150,
+        150,
+        0,
+        0,
+        30,
+        './images/spider.png'
+    );
 
-    canvas.addEventListener('click', () => createExplosion(main.explosion, 100, 100, main.spider));
-    canvas.addEventListener('mousemove', setAim);
+    const expl = new Image();
+    expl.src = './images/expl.jpg';
 
-};
+    const grassImg = new Image();
+    grassImg.src = './images/grass.jpg';
 
+    const explosion = [];
+    let anim;
 
+    canvas.addEventListener('mousemove', (e) => setAim(e, aim));
+    canvas.addEventListener('click', (e) => {
+        createExplosion(e, explosion, 100, 100, spider);
+    });
 
-function animation() {
-    update();
-    render();
-    main.anim = requestAnimationFrame(animation);
-};
-
-
-
-function render() {
-    main.ctx.drawImage(main.grassImg, 0, 0, 700, 463, 0, 0, main.canvas.width, main.canvas.height);
-
-    if (main.spider.timer > 0) {
-
-        renderSprite(main.spider.img, 128, 128, main.spider.x, main.spider.y, main.spider.w, main.spider.h, main.spider.animX, main.spider.animY);
-
-    }
-
-    renderExplosion(main.explosion, main.expl, 341.3, 364);
-    drawAim();
-};
-
-
-
-function update() {
-    if (main.spider.timer > 0) {
-        updateSprite(main.spider, 32, 8, 0.3);
-        main.spider.timer--;
-    } else {
-        main.spider.x = Math.random() * (canvas.width - 200);
-        main.spider.y = Math.random() * (canvas.height - 150);
-        main.spider.timer = 30;
-    }
-
-    updateExplosion(main.explosion, 3, 2, 0.3);
-};
-
-
-
-function playAnimation() {
-    init();
     animation();
-};
 
+
+    function animation() {
+        if (spider.hp) {
+            update();
+            render();
+            anim = requestAnimationFrame(animation);
+        } else {
+            const play = confirm('You Win!!! Play Again?');
+            if (play) {
+                spider.w = 150;
+                spider.hp = true;
+                anim = requestAnimationFrame(animation);
+            } else {
+                cancelAnimationFrame(anim)
+            }
+        }
+    };
+
+    function render() {
+        ctx.drawImage(grassImg, 0, 0, canvas.width, canvas.height);
+        if (spider.timer > 0) {
+            renderSprite(ctx, spider.img, 128, 128, spider.x, spider.y, spider.w, spider.h, spider.animX, spider.animY);
+        }
+        renderExplosion(ctx, explosion, expl, 341.3, 364);
+        drawAim(ctx, aim);
+    };
+
+    function update() {
+        if (spider.timer > 0) {
+            updateSprite(spider, 32, 8, 0.3);
+            spider.timer--;
+        } else {
+            // spider.x = Math.random() * (canvas.width - 200);
+            // spider.y = Math.random() * (canvas.height - 150);
+            spider.timer = 30;
+        }
+        updateExplosion(explosion, 3, 2, 0.3);
+    };
+}
--- scripts/megaNeura.js
+++ scripts/megaNeura.js
@@ -1,7 +1,5 @@
 'use strict'
-
-// create sprite start
-class SpriteMG {
+class Spider {
 	constructor(x, y, w, h, animX, animY, timerAnim, src) {
 		this.x = x;
 		this.y = y;
@@ -12,38 +10,100 @@ class SpriteMG {
 		this.timerAnim = timerAnim;
 		this.img = new Image();
 		this.img.src = src;
+		this.hp = true;
 	}
 
 }
-// create sprite end
+
+function drawAim(ctx, aim) {
+	ctx.save();
+
+	ctx.fillStyle = 'rgba(219, 20, 20, 1)';
+	ctx.strokeStyle = 'rgba(219, 20, 20, 1)';
+	ctx.lineWidth = 2;
 
 
-// explosion start
-function createExplosion(explosion, w, h, spider, dx = 0, dy = 0, myX, myY) {
-	if (event.pageX >= spider.x
-		&& event.pageX <= spider.x + spider.w
-		&& event.pageY >= spider.y
-		&& event.pageY <= spider.y + spider.h) {
-		myX = myX || event.pageX - canvas.getBoundingClientRect().x - document.documentElement.scrollLeft;
-		myY = myY || event.pageY - canvas.getBoundingClientRect().y - document.documentElement.scrollTop;
+	ctx.beginPath();
+	ctx.arc(aim.x, aim.y, 2, 0, 2 * Math.PI);
+	ctx.fill();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x - 50, aim.y - 15);
+	ctx.lineTo(aim.x - 50, aim.y - 25);
+	ctx.lineTo(aim.x - 35, aim.y - 25);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x + 50, aim.y - 15);
+	ctx.lineTo(aim.x + 50, aim.y - 25);
+	ctx.lineTo(aim.x + 35, aim.y - 25);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x + 50, aim.y + 15);
+	ctx.lineTo(aim.x + 50, aim.y + 25);
+	ctx.lineTo(aim.x + 35, aim.y + 25);
+	ctx.stroke();
+
+	ctx.moveTo(aim.x - 35, aim.y + 25);
+	ctx.beginPath();
+	ctx.lineTo(aim.x - 50, aim.y + 25);
+	ctx.lineTo(aim.x - 50, aim.y + 15);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x - 30, aim.y);
+	ctx.lineTo(aim.x - 50, aim.y);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x + 30, aim.y);
+	ctx.lineTo(aim.x + 50, aim.y);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x, aim.y - 15);
+	ctx.lineTo(aim.x, aim.y - 25);
+	ctx.stroke();
+
+	ctx.beginPath();
+	ctx.moveTo(aim.x, aim.y + 15);
+	ctx.lineTo(aim.x, aim.y + 25);
+	ctx.stroke();
+
+	ctx.restore();
+}
+
+function createExplosion(e, explosion, w, h, spider) {
+
+	const eX = e.pageX - canvas.getBoundingClientRect().x - document.documentElement.scrollLeft;
+	const eY = e.pageY - canvas.getBoundingClientRect().y - document.documentElement.scrollTop;
+
+	if (eX >= spider.x
+		&& eX <= spider.x + spider.w
+		&& eY >= spider.y
+		&& eY <= spider.y + spider.h) {
 		explosion.push({
-			x: myX - w / 2 + dx,
-			y: myY - h / 2 + dy,
+			x: eX - w / 2,
+			y: eY - h / 2,
 			w,
 			h,
 			animX: 0,
 			animY: 0
 		});
-		spider.w = 0;
+		spider.w = 0; 
+		setTimeout(() => {
+			spider.hp = false;
+		}, 1000);
 	}
 
 };
 
-function renderExplosion(explosion, explImg, w, h) {
+function renderExplosion(ctx, explosion, explImg, w, h) {
 	for (let i = 0; i < explosion.length; i++) {
-		main.ctx.drawImage(
+		ctx.drawImage(
 			explImg, w * Math.floor(explosion[i].animX),
-			h * Math.floor(explosion[i].animY), w, h,
+			h * explosion[i].animY, w, h,
 			explosion[i].x, explosion[i].y, explosion[i].w, explosion[i].h
 		);
 	};
@@ -57,16 +117,13 @@ function updateExplosion(explosion, gor, vert, vel = 1) {
 			explosion[i].animX = 0;
 		}
 		if (explosion[i].animY >= vert) {
-			explosion.splice(i, 1);
+			explosion.splice(i--, 1);
 		}
 	}
 };
 
-// explosion end
-
-// animation object start
-function renderSprite(spriteImg, segmentWidth, segmentHeight, spriteX, spriteY, spriteWidth, spriteHeight, spriteAnimX, spriteAnimY) {
-	main.ctx.drawImage(
+function renderSprite(ctx, spriteImg, segmentWidth, segmentHeight, spriteX, spriteY, spriteWidth, spriteHeight, spriteAnimX, spriteAnimY) {
+	ctx.drawImage(
 		spriteImg, segmentWidth * Math.floor(spriteAnimX),
 		segmentHeight * Math.floor(spriteAnimY), segmentWidth, segmentHeight,
 		spriteX, spriteY, spriteWidth, spriteHeight
@@ -83,4 +140,8 @@ function updateSprite(sprite, gor, vert, vel = 1) {
 		sprite.animY = 0;
 	}
 };
-// animation object end
+
+function setAim(event, aim) {
+	aim.x = event.pageX - canvas.getBoundingClientRect().x - document.documentElement.scrollLeft;
+	aim.y = event.pageY - canvas.getBoundingClientRect().y - document.documentElement.scrollTop;
+}
